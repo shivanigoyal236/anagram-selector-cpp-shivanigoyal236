9bfdcb10dd8d0362e129eae92f7c6443a1c4cc0d
shivanigoyal <38599629+shivanigoyal236@users.noreply.github.com>
2020-09-15 12:40:56 +0530

    Update anagram.h

--- anagram.h
+++ anagram.h
@@ -1,11 +1,44 @@
-#include <string>
-#include <vector>
+#include<bits/stdc++.h>
+#include<iostream> 
+#include<string.h>
+#include <sstream>
+using namespace std;
 
-namespace Anagram
+class CheckValues
 {
-    bool WordPairIsAnagram(const std::string& word1, const std::string& word2);
+     public:
+   CheckValues(){}
+    string CheckValuesForAlert(float value,string &ValueName,float HighValueForAlert,float HighValueForWarning,float LowValueForAlert,float LowValueForWarning);
+    string checkforlowvalues(float value,string &ValueName,float LowValueForAlert,float LowValueForWarning);
+    string checkforhighvalues(float value,string &ValueName,float HighValueForAlert,float HighValueForWarning);
+};
+class PrintMessage
+{   
+    public:
+    void PrintMessageOnConsole(string &messageForTem,string &messageForHumi);
+};
 
-    std::vector<std::string> SelectAnagrams(
-        const std::string& word,
-        const std::vector<std::string>& candidates);
-} // namespace Anagram
+class CheckForWarningAndAlert
+{     
+ private:
+    CheckValues check;
+    PrintMessage *print;
+    float ErrorHighValueForTem=40;
+    float ErrorLowValueForTem=0;
+    float WarningHighValueForTem=37;
+    float WarningLowValueFortem=4;
+    float ErrorHighValueForHumi=90;
+    float WarningHighValueForHumi=70;
+    public:
+    CheckForWarningAndAlert(){}
+     CheckForWarningAndAlert(float tempreature,float humidity)
+     {
+         string humidityname="humidity",tempreaturename="tempreature";
+     string messageForTem=check.CheckValuesForAlert(tempreature,tempreaturename,ErrorHighValueForTem
+     ,WarningHighValueForTem,ErrorLowValueForTem,WarningLowValueFortem);
+     string messageForHumi=check.CheckValuesForAlert(humidity,humidityname,ErrorHighValueForHumi
+     ,WarningHighValueForHumi,0,0);
+     print->PrintMessageOnConsole(messageForTem,messageForHumi);
+     }
+    
+};
